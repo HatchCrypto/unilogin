c6237e8d3a6485bb0c8fc1e7ef44ae6cdf385bdb
Howard O'Leary <howardeoleary@gmail.com>
2018-05-29 10:44:00 -0500

    styled component start

--- src/components/Eip1077.js
+++ src/components/Eip1077.js
@@ -31,40 +31,22 @@ class Eip1077 extends Component {
 
     render(){
     return(
-    <div>
-
-    //  1) Using the styling of EIP1077Credentials-Name, please make the input below a "sign-up" button
-    //  that intiates the creation of the EIP1077 Credentials using the inputted string as the localStorage
-    // setItem name.
-
-    <div>
-    <p>Create a new login ID:</p>
-    <input type="text" value=""/>
-    <button onClick={this.sendTransaction}>Send</button>
-    </div>
-
-   // 2) Below, display a JSON object containing login ID, the Ethereum address and public key
-   // that will be used to generate a user ID contract
-
+    <div className="container">
+      <div className="row">
+        <p>Create a new login ID:</p>
+        <input type="text" value=""/>
+        <button onClick={this.sendTransaction}>Send</button>
+      </div>
      <div>
-     <p>Send your new account and public key to the factory contract:</p>
-     <input type="button" value="Create User ID contract"/>
+      <p>Send your new account and public key to the factory contract:</p>
+      <input type="button" value="Create User ID contract"/>
      </div>
-
-    // 3) Using the options below, generate a data payload that contains the
-    // desired 'action to execute', the hash of that payload, and the signed hash
-    // of that payload.
-    //
-    // To complete this, you must use PrivateKeySign component along with the EIP1077 payload
-    // component (still needs to be added)
-
-    <div>
-    <p>What action would you like to send along?</p>
-    <input type="button" value="READ()"/>
-    <input type="button" value="WRITE()"/>
-    <input type="button" value="PING()"/>
-    </div>
-
+      <div>
+        <p>What action would you like to send along?</p>
+        <input type="button" value="READ()"/>
+        <input type="button" value="WRITE()"/>
+        <input type="button" value="PING()"/>
+      </div>
     </div>
     )}
 }
