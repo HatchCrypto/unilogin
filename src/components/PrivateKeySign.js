67a24031a5dd8a4744aa4fcefae8131ba44bf90d
Coogan <coogan.brennan@gmail.com>
2018-05-28 22:41:30 -0500

    added private key signing

---
+++ src/components/PrivateKeySign.js
@@ -0,0 +1,19 @@
+
+/**
+ * @PrivateKeySign
+ * Takes a private key and a data payload, signs the data with private key
+ *
+ * Returns a signed and unsigned data payload
+ */
+import ethUtils from 'ethereumjs-util';
+
+const PrivateKeySign = ( _data, _account, _privateKey ) => {
+  const header = '0x19'
+  const version = '0'
+  const _transactionHash = ethUtils.keccak256(header, version, _account, _data);
+  const _signedTransactionHash = ethUtils.ecsign(_transactionHash, _privateKey);
+
+  var item = {transactionHash: _transactionHash, signedTransactionHash: _signedTransactionHash};
+}
+
+export default PrivateKeySign;
